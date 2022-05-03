7df255d72230dadf02f5a2d9fa2033e4576b9860
Marlon Duarte <eletromarlon@gmail.com>
2022-05-03 12:54:00 -0300

    Adding unfinished project

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import mkTokens
 import mkComents
+import testeComentarioOK
 #import analisador
 arq = open("teste.txt")
 arquivo = arq.read()
 linhas = arq.readlines()
 
+comentFuncionando = testeComentarioOK.verifyComent(arquivo)
 
-#print(arquivo)
-
-teste = mkComents.verificaComents(arquivo)
+print("\n\nRestante do codigo sem o coment: \n", comentFuncionando)
 
 #Antes de mandar para analisador, devo mandar para verificador de comentario mkComents.
 
-'''cabecalho = mkTokens.verificaInt(linhas)
-pontuacao = mkTokens.verificaPontuacao(linhas)
-atribuicao = mkTokens.verificaAtribuicao(linhas)
-funcao = mkTokens.verificaMain(linhas)
-negacao = mkTokens.verificaNegacao(linhas)
+cabecalho = mkTokens.verificaInt(comentFuncionando.split("\n"))
+pontuacao = mkTokens.verificaPontuacao(comentFuncionando.split("\n"))
+atribuicao = mkTokens.verificaAtribuicao(comentFuncionando.split("\n"))
+funcao = mkTokens.verificaMain(comentFuncionando.split("\n"))
+negacao = mkTokens.verificaNegacao(comentFuncionando.split("\n"))
 
 for n in cabecalho:
     print(n)
@@ -28,8 +28,10 @@ for n in funcao:
     print(n)
 for n in negacao:
     print(n)
-    '''
-    
+
+teste = mkComents.verifyOneComents(comentFuncionando)
+
+print(teste)
 #for linha in linhas:
 #    print(linha)
 
--- mkComents.py
+++ mkComents.py
@@ -1,31 +1,55 @@
-def verificaComents(linguica):
-    i = 0
-    j = 0
-    startComent = 0
-    finishComent = 0
-    coment = ""
+def verifyOneComents(linguica):
+    l = 0
+    k = 0
+
+    for m in linguica:
+        if m == '#' and l == 0:
+            k += 1
     
-    for n in linguica:
-        if n == '#' and i < 1:
-            startComent = j
-            i += 1
-            #print(n, j, startComent)
-        else:
-            if n == '#' and i > 0:
-                finishComent = j
-            #print(n, j, finishComent)
-        j += 1
+    l += 1
+       
+    #print("l: ",l ," k: ",k ," i: ", i," j: " ,j  )
     
-    coment = linguica[(startComent + 1):(finishComent)]
+    def verifyComent(linguica):
+        i = 0
+        j = 0
+     
+        startComent = 0
+        finishComent = 0
+        coment = ""
+        
+        print("passou")
+        
+        for n in linguica:
+            if n == '#' and i < 1:
+                startComent = j
+                i += 1
+                #print(n, j, startComent)
+            else:
+                if n == '#' and i > 0:
+                    finishComent = j
+                    coment = linguica[(startComent + 1):(finishComent)]
+                    linguica = linguica[:startComent] + linguica[finishComent + 1:]
+                    print(coment, "posicao ", j)
+                    if k / 2 != 0:
+                        print(linguica)
+                        verifyComent(linguica)
+                    else:
+                        if k % 2 != 0:
+                            print("Erro")
+                            return -1  
+                    #print(linguica)
+                    return linguica 
+                
+                #print(n, j, finishComent)
+            j += 1
+    return "Erro Geral em mkComents.py"
     
-    linguica = linguica[:startComent] + linguica[finishComent + 1:]
     
     # j = startComent
     # for k in range(finishComent - startComent):
     #     linguica.pop(0)
     #     j += 1 
-    
-    print(coment, "\n" ,linguica)
     
     # for m in code:
     #     sizeM = m.length()
